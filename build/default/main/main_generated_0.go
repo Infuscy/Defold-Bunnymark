be47fcfece24621b17b4010855512b497c211a7e
Infuscy <buse.florinx@gmail.com>
2016-06-04 02:07:24 +0300

    Init project.

---
+++ build/default/main/main_generated_0.go
@@ -0,0 +1,45 @@
+components {
+  id: "factory"
+  component: "/main/bunny.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "gui"
+  component: "/main/fps.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script"
+  component: "/main/main.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
