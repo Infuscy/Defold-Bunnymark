9d6789f7d1fd75a60977a10fb7e77675f78a1e02
Infuscy <buse.florinx@gmail.com>
2016-06-04 16:57:01 +0300

    FURTHER OPTIM.

--- build/default/main/main_generated_0.go
+++ build/default/main/main_generated_0.go
@@ -14,21 +14,6 @@ components {
   }
 }
 components {
-  id: "gui"
-  component: "/main/fps.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-components {
   id: "script"
   component: "/main/main.script"
   position {
